7067c1013882eb8ee95a6ea69aed700b1a233bfe
qiandongzhao <987889957@qq.com>
2022-05-31 23:16:04 +0800

    commit

--- app/src/main/java/com/rihdkauecgh/plihgnytrvfws/ui/HomePageActivity.java
+++ app/src/main/java/com/rihdkauecgh/plihgnytrvfws/ui/HomePageActivity.java
@@ -32,7 +32,7 @@ public class HomePageActivity extends XActivity<MainPresent> {
 
     private long exitTime = 0;
     private List<Fragment> mFragments = new ArrayList<>();
-    private String[] mTitles = {"首页", "产品", "我的"};
+    private String[] mTitles = {"首页", "精选", "设置"};
     private int[] uncheckedIcon = {R.drawable.footer_icon_n_sy, R.drawable.footer_icon_n_cp, R.drawable.footer_icon_n_wd};
     private int[] checkedIcon = {R.drawable.footer_icon_f_sy, R.drawable.footer_icon_f_cp, R.drawable.footer_icon_f_wd};
     private ArrayList<CustomTabEntity> customTabEntities;
