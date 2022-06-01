c91905363162782eff31089070eb1b2c1eb1e0f3
qiandongzhao <987889957@qq.com>
2022-06-01 14:27:08 +0800

    下载apk并安装

--- app/src/main/java/com/aklsfasad/fsjhfkk/ui/WebHuiMinActivity.java
+++ app/src/main/java/com/aklsfasad/fsjhfkk/ui/WebHuiMinActivity.java
@@ -1,20 +1,35 @@
 package com.aklsfasad.fsjhfkk.ui;
 
+import android.Manifest;
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 
+import android.os.Environment;
+import android.provider.Settings;
+import android.util.Log;
 import android.view.KeyEvent;
 import android.view.ViewGroup;
+import android.webkit.DownloadListener;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import androidx.core.content.FileProvider;
+
 import com.aklsfasad.fsjhfkk.R;
 import com.aklsfasad.fsjhfkk.utils.StatusBarUtilHuiMin;
 import com.aklsfasad.fsjhfkk.mvp.XActivity;
+import com.aklsfasad.fsjhfkk.widget.DownloadUtil;
+
+import java.io.File;
 
 import butterknife.BindView;
+import pub.devrel.easypermissions.EasyPermissions;
 
 public class WebHuiMinActivity extends XActivity {
 
@@ -33,9 +48,14 @@ public class WebHuiMinActivity extends XActivity {
 
     private WebSettings webSettings;
 
+    protected static final int RC_PERM = 123;
+
+    private String filePath;
+
     @Override
     public void initData(Bundle savedInstanceState) {
         StatusBarUtilHuiMin.setTransparent(this, false);
+        checkPermission();
         bundle = getIntent().getExtras();
         if (bundle.containsKey("tag"))
             tag = bundle.getInt("tag");
@@ -60,6 +80,109 @@ public class WebHuiMinActivity extends XActivity {
         webSettings.setTextZoom(100);
         webView.setWebViewClient(new WebViewClient());
         webView.loadUrl(url);
+        webView.setDownloadListener(new DownloadListener() {
+            @Override
+            public void onDownloadStart(String url, String userAgent, String contentDisposition, String mimetype, long contentLength) {
+                Log.d("WebViewActivity", "url = " + url + "--> userAgent" + userAgent + " ---> contentDisposition = " + contentDisposition + "-->mimetype = " + mimetype + "-->contentLength = " + contentLength);
+                downFile(url);
+            }
+        });
+    }
+
+    public void downFile(String url) {
+        ProgressDialog progressDialog = new ProgressDialog(WebHuiMinActivity.this);
+        progressDialog.setProgressStyle(ProgressDialog.STYLE_HORIZONTAL);
+        progressDialog.setTitle("下载中...");
+        progressDialog.setProgress(0);
+        progressDialog.setMax(100);
+        progressDialog.show();
+        progressDialog.setCancelable(false);
+        String apkName[] = url.split("/");
+        DownloadUtil.get().download(url, Environment.getExternalStorageDirectory().getAbsolutePath() + "/Download/", apkName[apkName.length - 1], new DownloadUtil.OnDownloadListener() {
+            @Override
+            public void onDownloadSuccess(File file) {
+                if (progressDialog != null && progressDialog.isShowing()) {
+                    progressDialog.dismiss();
+                }
+                //下载完成进行相关逻辑操作
+                Log.e("====", "=======>" + file.getPath());
+                filePath = file.getPath();
+                if (Build.VERSION.SDK_INT < Build.VERSION_CODES.O) {
+                    // Android8.0之前，直接安装Apk
+                    installApk();
+                    return;
+                }
+                boolean haveInstallPermission = getPackageManager().canRequestPackageInstalls();
+                if (!haveInstallPermission) {
+                    // 权限没有打开则提示用户去手动打开
+                    Uri packageURI = Uri.parse("package:" + getPackageName());
+                    Intent intent = new Intent(Settings.ACTION_MANAGE_UNKNOWN_APP_SOURCES, packageURI);
+                    startActivityForResult(intent, 1001);
+                }
+
+            }
+
+            @Override
+            public void onDownloading(int progress) {
+                progressDialog.setProgress(progress);
+            }
+
+            @Override
+            public void onDownloadFailed(Exception e) {
+                //下载异常进行相关提示操作
+
+            }
+        });
+    }
+
+    /**
+     * 未知来源安装权限申请回调
+     */
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode != RESULT_OK) {
+            return;
+        }
+        if (requestCode == 1001 && Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            // 未知来源安装应用权限开启
+            boolean haveInstallPermission = getPackageManager().canRequestPackageInstalls();
+            if (haveInstallPermission) {
+                installApk();
+            }
+        }
+    }
+
+    /**
+     * 安装最新Apk
+     */
+    private void installApk() {
+        File file = new File(filePath);
+        try {
+            Intent intent = new Intent(Intent.ACTION_VIEW);
+            intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            intent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+                String packageName = context.getApplicationContext().getPackageName();
+                String authority = new StringBuilder(packageName).append(".provider").toString();
+                Uri uri = FileProvider.getUriForFile(context, authority, file);
+                intent.setDataAndType(uri, "application/vnd.android.package-archive");
+            } else {
+                Uri uri = Uri.fromFile(file);
+                intent.setDataAndType(uri, "application/vnd.android.package-archive");
+            }
+            context.startActivity(intent);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void checkPermission() {
+        String[] per = {Manifest.permission.WRITE_EXTERNAL_STORAGE, Manifest.permission.READ_EXTERNAL_STORAGE};
+        if (!EasyPermissions.hasPermissions(this, per)) {
+            EasyPermissions.requestPermissions(this, "需要允许读写内存卡权限",
+                    RC_PERM, per);
+        }
     }
 
     @Override
