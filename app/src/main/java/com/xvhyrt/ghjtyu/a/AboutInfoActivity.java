b6b9cb0752f7db9ae7ef5a35736f471493e41876
qiandongzhao <987889957@qq.com>
2022-06-10 23:11:38 +0800

    commit

--- app/src/main/java/com/xvhyrt/ghjtyu/a/AboutInfoActivity.java
+++ app/src/main/java/com/xvhyrt/ghjtyu/a/AboutInfoActivity.java
@@ -7,7 +7,9 @@ import android.widget.TextView;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.xvhyrt.ghjtyu.MainApp;
 import com.xvhyrt.ghjtyu.R;
+import com.xvhyrt.ghjtyu.u.OpenUtil;
 import com.xvhyrt.ghjtyu.u.StatusBarUtil;
 
 public class AboutInfoActivity extends AppCompatActivity {
@@ -20,6 +22,8 @@ public class AboutInfoActivity extends AppCompatActivity {
         ImageView backImg = findViewById(R.id.back_image);
         backImg.setOnClickListener(v -> finish());
         TextView textView = findViewById(R.id.biaoti_tv);
+        TextView version_code_tv = findViewById(R.id.version_code_tv);
+        version_code_tv.setText("当前版本号：v" + OpenUtil.getAppVersion(MainApp.getContext()));
         textView.setText("关于我们");
     }
 }
