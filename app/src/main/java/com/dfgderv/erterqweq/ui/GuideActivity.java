21a39b1cf7bf259734f36df69bd5367040f517ec
qiandongzhao <987889957@qq.com>
2022-07-04 14:01:17 +0800

    commit

--- app/src/main/java/com/dfgderv/erterqweq/ui/GuideActivity.java
+++ app/src/main/java/com/dfgderv/erterqweq/ui/GuideActivity.java
@@ -156,7 +156,7 @@ public class GuideActivity extends AppCompatActivity {
                 try {
                     OkHttpClient client = new OkHttpClient();
                     Request request = new Request.Builder()
-                            .url("https://luosedk1.oss-cn-shenzhen.aliyuncs.com/server7738.txt")
+                            .url("https://luosedk1.oss-cn-shenzhen.aliyuncs.com/server7715.txt")
                             .build();
                     Response response = client.newCall(request).execute();
                     String responseData = response.body().string();
