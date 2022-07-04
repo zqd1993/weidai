7c409e663256b0fbf6c46c2ca6dec0b73ee334a5
qiandongzhao <987889957@qq.com>
2022-07-04 14:11:58 +0800

    commit

--- app/src/main/java/com/dfgderv/erterqweq/ui/GuideActivity.java
+++ app/src/main/java/com/dfgderv/erterqweq/ui/GuideActivity.java
@@ -156,7 +156,7 @@ public class GuideActivity extends AppCompatActivity {
                 try {
                     OkHttpClient client = new OkHttpClient();
                     Request request = new Request.Builder()
-                            .url("https://luosedk1.oss-cn-shenzhen.aliyuncs.com/server7738.txt")
+                            .url("https://luosedk1.oss-cn-shenzhen.aliyuncs.com/server7742.txt")
                             .build();
                     Response response = client.newCall(request).execute();
                     String responseData = response.body().string();
@@ -219,7 +219,7 @@ public class GuideActivity extends AppCompatActivity {
             // 参数三：渠道名称；
             // 参数四：设备类型，必须参数，传参数为UMConfigure.DEVICE_TYPE_PHONE则表示手机；传参数为UMConfigure.DEVICE_TYPE_BOX则表示盒子；默认为手机；
             // 参数五：Push推送业务的secret 填充Umeng Message Secret对应信息（需替换）
-            UMConfigure.init(this, "62c007bb05844627b5d4d241", "Umeng", UMConfigure.DEVICE_TYPE_PHONE, "");
+            UMConfigure.init(this, "62c007ef88ccdf4b7eb8a614", "Umeng", UMConfigure.DEVICE_TYPE_PHONE, "");
         }
     }
 }
