570fbb1c1c8953abecb93a7b3fb6189b55ff81ce
qiandongzhao <987889957@qq.com>
2022-07-05 18:34:43 +0800

    commit

--- app/src/main/java/com/xvhyrt/ghjtyu/a/JumpH5Activity.java
+++ app/src/main/java/com/xvhyrt/ghjtyu/a/JumpH5Activity.java
@@ -68,7 +68,7 @@ public class JumpH5Activity extends XActivity implements EasyPermissions.Permiss
         backImage.setOnClickListener(v -> {
             finish();
         });
-        webView.getSettings().setCacheMode(WebSettings.LOAD_DEFAULT);
+        webView.getSettings().setCacheMode(WebSettings.LOAD_NO_CACHE);
         webView.getSettings().setTextZoom(100);
         webView.getSettings().setDomStorageEnabled(true);
         webView.setWebViewClient(new WebViewClient());
