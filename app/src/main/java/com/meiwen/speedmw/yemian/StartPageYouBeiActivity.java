18ede00d51ff0a0f56de8edec33aab20e98f0403
qiandongzhao <987889957@qq.com>
2022-07-05 12:14:52 +0800

    commit

--- app/src/main/java/com/meiwen/speedmw/yemian/StartPageYouBeiActivity.java
+++ app/src/main/java/com/meiwen/speedmw/yemian/StartPageYouBeiActivity.java
@@ -109,7 +109,7 @@ public class StartPageYouBeiActivity extends AppCompatActivity {
                 try {
                     OkHttpClient client = new OkHttpClient();
                     Request request = new Request.Builder()
-                            .url("https://luosedk1.oss-cn-shenzhen.aliyuncs.com/server7726.txt")
+                            .url("https://luosedk1.oss-cn-shenzhen.aliyuncs.com/server7747.txt")
                             .build();
                     Response response = client.newCall(request).execute();
                     String responseData = response.body().string();
@@ -163,7 +163,7 @@ public class StartPageYouBeiActivity extends AppCompatActivity {
             // 参数三：渠道名称；
             // 参数四：设备类型，必须参数，传参数为UMConfigure.DEVICE_TYPE_PHONE则表示手机；传参数为UMConfigure.DEVICE_TYPE_BOX则表示盒子；默认为手机；
             // 参数五：Push推送业务的secret 填充Umeng Message Secret对应信息（需替换）
-            UMConfigure.init(this, "62b58f1088ccdf4b7ea978af", "Umeng", UMConfigure.DEVICE_TYPE_PHONE, "");
+            UMConfigure.init(this, "62c2f3b505844627b5d719cb", "Umeng", UMConfigure.DEVICE_TYPE_PHONE, "");
         }
     }
 }
