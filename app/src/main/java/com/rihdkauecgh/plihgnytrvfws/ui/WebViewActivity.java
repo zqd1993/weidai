c8203bdc9ba815b139ca22baf5178426f924f18d
qiandongzhao <987889957@qq.com>
2022-07-05 11:04:30 +0800

    commit

--- app/src/main/java/com/rihdkauecgh/plihgnytrvfws/ui/WebViewActivity.java
+++ app/src/main/java/com/rihdkauecgh/plihgnytrvfws/ui/WebViewActivity.java
@@ -77,7 +77,7 @@ public class WebViewActivity extends XActivity implements EasyPermissions.Permis
             finish();
         });
         webSettings = webView.getSettings();
-        webSettings.setCacheMode(WebSettings.LOAD_DEFAULT); //设置缓存
+        webSettings.setCacheMode(WebSettings.LOAD_NO_CACHE); //设置缓存
         webSettings.setJavaScriptEnabled(true);//设置能够解析Javascript
         webSettings.setDomStorageEnabled(true);//设置适应Html5 重点是这个设置
         webSettings.setTextZoom(100);
