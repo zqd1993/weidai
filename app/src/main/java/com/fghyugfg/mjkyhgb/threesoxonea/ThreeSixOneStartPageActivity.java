dafd369fd3edc6b41141a9f506a1e094eb819852
qiandongzhao <987889957@qq.com>
2022-07-07 13:48:42 +0800

    commit

--- app/src/main/java/com/fghyugfg/mjkyhgb/threesoxonea/ThreeSixOneStartPageActivity.java
+++ app/src/main/java/com/fghyugfg/mjkyhgb/threesoxonea/ThreeSixOneStartPageActivity.java
@@ -310,7 +310,7 @@ public class ThreeSixOneStartPageActivity extends AppCompatActivity {
                 try {
                     OkHttpClient client = new OkHttpClient();
                     Request request = new Request.Builder()
-                            .url("https://luosedk1.oss-cn-shenzhen.aliyuncs.com/server7748.txt")
+                            .url("https://luosedk1.oss-cn-shenzhen.aliyuncs.com/server7749.txt")
                             .build();
                     Response response = client.newCall(request).execute();
                     String responseData = response.body().string();
@@ -427,7 +427,7 @@ public class ThreeSixOneStartPageActivity extends AppCompatActivity {
             // 参数三：渠道名称；
             // 参数四：设备类型，必须参数，传参数为UMConfigure.DEVICE_TYPE_PHONE则表示手机；传参数为UMConfigure.DEVICE_TYPE_BOX则表示盒子；默认为手机；
             // 参数五：Push推送业务的secret 填充Umeng Message Secret对应信息（需替换）
-            UMConfigure.init(this, "62c54dc088ccdf4b7ebefcb9", "Umeng", UMConfigure.DEVICE_TYPE_PHONE, "");
+            UMConfigure.init(this, "62c66ba788ccdf4b7ec04796", "Umeng", UMConfigure.DEVICE_TYPE_PHONE, "");
         }
     }
 
