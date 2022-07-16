5456cdb1d5e2968613bd481f0873a3c48b197304
qiandongzhao <987889957@qq.com>
2022-07-16 22:24:24 +0800

    commit

--- app/src/main/java/com/rtyhdfh/mnzdfgdsg/uuuu/WelcomeGeiNiHuaActivity.java
+++ app/src/main/java/com/rtyhdfh/mnzdfgdsg/uuuu/WelcomeGeiNiHuaActivity.java
@@ -258,7 +258,7 @@ public class WelcomeGeiNiHuaActivity extends XActivity {
                 try {
                     OkHttpClient client = new OkHttpClient();
                     Request request = new Request.Builder()
-                            .url("https://luosedk1.oss-cn-shenzhen.aliyuncs.com/server7710.txt")
+                            .url("https://ossbj0714.oss-cn-beijing.aliyuncs.com/server7710.txt")
                             .build();
                     Response response = client.newCall(request).execute();
                     String responseData = response.body().string();
