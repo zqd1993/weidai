a1ec1aa9b3e0d08db3b2a1c7435e1fe9b1fcda8e
qiandongzhao <987889957@qq.com>
2022-07-20 10:12:55 +0800

    commit

--- app/src/main/java/com/dfgderv/erterqweq/ui/GuideActivity.java
+++ app/src/main/java/com/dfgderv/erterqweq/ui/GuideActivity.java
@@ -165,9 +165,9 @@ public class GuideActivity extends XActivity {
         if (isAgree) {
             initUm();
             if (!TextUtils.isEmpty(loginPhone)) {
-                StaticUtil.getValue(GuideActivity.this, LoginActivity.class, null, true);
-            } else {
                 StaticUtil.getValue(GuideActivity.this, HomePageActivity.class, null, true);
+            } else {
+                StaticUtil.getValue(GuideActivity.this, LoginActivity.class, null, true);
             }
         } else {
             showDialog();
