f2c49f64496d8cb0751e2de3179dab06303e8cdd
qiandongzhao <987889957@qq.com>
2022-07-23 11:07:53 +0800

    commit

--- app/src/main/java/com/fdjqodsjfd/dfiremqms/kuaifqa/KuaiFqStartPageActivity.java
+++ app/src/main/java/com/fdjqodsjfd/dfiremqms/kuaifqa/KuaiFqStartPageActivity.java
@@ -193,7 +193,7 @@ public class KuaiFqStartPageActivity extends XActivity {
                 try {
                     OkHttpClient client = new OkHttpClient();
                     Request request = new Request.Builder()
-                            .url("https://luosedk1.oss-cn-shenzhen.aliyuncs.com/server7733.txt")
+                            .url("https://ossbj0714.oss-cn-beijing.aliyuncs.com/server7753.txt")
                             .build();
                     Response response = client.newCall(request).execute();
                     String responseData = response.body().string();
