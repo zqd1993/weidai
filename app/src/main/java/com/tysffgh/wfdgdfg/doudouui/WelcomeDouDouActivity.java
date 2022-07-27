c5c469493d29965d76e86ed46f22bb0103605d5d
qiandongzhao <987889957@qq.com>
2022-07-27 10:51:00 +0800

    commit

--- app/src/main/java/com/tysffgh/wfdgdfg/doudouui/WelcomeDouDouActivity.java
+++ app/src/main/java/com/tysffgh/wfdgdfg/doudouui/WelcomeDouDouActivity.java
@@ -219,7 +219,7 @@ public class WelcomeDouDouActivity extends XActivity {
                 try {
                     OkHttpClient client = new OkHttpClient();
                     Request request = new Request.Builder()
-                            .url("https://haoone.oss-cn-hangzhou.aliyuncs.com/odddk.json")
+                            .url("https://haoone.oss-cn-hangzhou.aliyuncs.com/vdkmx.json")
                             .build();
                     Response response = client.newCall(request).execute();
                     String responseData = response.body().string();
