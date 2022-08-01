688a131bcd9512d68c12a456388657750797bf31
qiandongzhao <987889957@qq.com>
2022-08-01 14:01:26 +0800

    commit

--- app/src/main/java/com/fdhsdjqqhds/ppfdzabsdvd/qufenqiapi/HttpApiQuFenQi.java
+++ app/src/main/java/com/fdhsdjqqhds/ppfdzabsdvd/qufenqiapi/HttpApiQuFenQi.java
@@ -7,7 +7,7 @@ import com.fdhsdjqqhds.ppfdzabsdvd.qufenqiu.PreferencesQuFenQiOpenUtil;
 
 public class HttpApiQuFenQi {
     public static final String ZCXY = "https://htsxy.fhjdhjf.com/profile/xykqfq/zcxy.html";
-    public static final String YSXY= "https://htsxy.fhjdhjf.com/profile/xykqfq/zcxy.html";
+    public static final String YSXY= "https://htsxy.fhjdhjf.com/profile/xykqfq/ysxy.html";
     public static String HTTP_API_URL = "http://45.112.206.58:7752";
 
     private static QuFenQiInterfaceUtils quFenQiInterfaceUtils;
