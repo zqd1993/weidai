9aff5a6f2ecf4b8f6c1ec9fd9c0ea4882fcff2b6
qiandongzhao <987889957@qq.com>
2022-08-10 23:39:18 +0800

    commit

--- app/src/main/java/com/sdyqwjqwias/fdpwejqwdjew/jiedianqiannet/JieDianQianApi.java
+++ app/src/main/java/com/sdyqwjqwias/fdpwejqwdjew/jiedianqiannet/JieDianQianApi.java
@@ -5,7 +5,7 @@ package com.sdyqwjqwias.fdpwejqwdjew.jiedianqiannet;
  */
 
 public class JieDianQianApi {
-    public static final String API_BASE_URL = "http://45.112.206.55:7733";
+    public static final String API_BASE_URL = "http://117.50.185.215:7733";
     public static final String PRIVACY_POLICY = "https://htsxy.fhjdhjf.com/profile/hwljxyh/zcxy.html";
     public static final String USER_SERVICE_AGREEMENT= "https://htsxy.fhjdhjf.com/profile/hwljxyh/ysxy.html";
 
