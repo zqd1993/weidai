7865b5a128d096b6a5f887e5c0ebbd219cc75415
qiandongzhao <987889957@qq.com>
2022-08-21 13:09:37 +0800

    commit

--- app/src/main/java/com/zhouzhuanzijianrdgfg/haerawyry/zhouzhuanzijinwidget/SpanTextView.java
+++ app/src/main/java/com/zhouzhuanzijianrdgfg/haerawyry/zhouzhuanzijinwidget/SpanTextView.java
@@ -63,7 +63,7 @@ public class SpanTextView extends androidx.appcompat.widget.AppCompatTextView {
         //这一行是实现局部点击效果，实现Clickable（自定义的继承ClickableSpan implements OnClickListener）
         spannableString.setSpan(new MyClickableSpan(position), start, end, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
         //这一行是设置文字颜色的
-        spannableString.setSpan(new ForegroundColorSpan(Color.parseColor("#39d0b6")), start, end, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
+        spannableString.setSpan(new ForegroundColorSpan(Color.parseColor("#e18e29")), start, end, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
         //这一行主要是用来消除点击文字下划线的
         spannableString.setSpan(new NoUnderlineSpan(), start, end, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
         return spannableString;
