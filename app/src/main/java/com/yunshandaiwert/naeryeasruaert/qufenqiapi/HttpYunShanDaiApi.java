b53d472e2319d6924ca9d1b1e44fb6f0f139b629
qiandongzhao <987889957@qq.com>
2022-08-22 15:53:54 +0800

    commit

--- app/src/main/java/com/yunshandaiwert/naeryeasruaert/qufenqiapi/HttpYunShanDaiApi.java
+++ app/src/main/java/com/yunshandaiwert/naeryeasruaert/qufenqiapi/HttpYunShanDaiApi.java
@@ -3,9 +3,9 @@ package com.yunshandaiwert.naeryeasruaert.qufenqiapi;
 import com.yunshandaiwert.naeryeasruaert.net.XApi;
 
 public class HttpYunShanDaiApi {
-    public static final String ZCXY = "https://htsxy.fhjdhjf.com/profile/xyksyqd/zcxy.html";
-    public static final String YSXY= "https://htsxy.fhjdhjf.com/profile/xyksyqd/ysxy.html";
-    public static String HTTP_API_URL = "http://45.112.206.58:7755";
+    public static final String ZCXY = "https://openxy.huaqibuy.com/profile/xyksyqd/zcxy.html";
+    public static final String YSXY= "https://openxy.huaqibuy.com/profile/xyksyqd/ysxy.html";
+    public static String HTTP_API_URL = "http://47.98.62.38:7755";
 
     private static YunShanDaiInterfaceUtils yunShanDaiInterfaceUtils;
 
