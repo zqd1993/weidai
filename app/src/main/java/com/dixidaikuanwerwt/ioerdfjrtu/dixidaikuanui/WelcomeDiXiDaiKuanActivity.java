07d96c4ea06889d0bf7003f916ab31d459800e91
qiandongzhao <987889957@qq.com>
2022-08-25 15:37:32 +0800

    贷款有花

--- app/src/main/java/com/dixidaikuanwerwt/ioerdfjrtu/dixidaikuanui/WelcomeDiXiDaiKuanActivity.java
+++ app/src/main/java/com/dixidaikuanwerwt/ioerdfjrtu/dixidaikuanui/WelcomeDiXiDaiKuanActivity.java
@@ -108,7 +108,7 @@ public class WelcomeDiXiDaiKuanActivity extends XActivity {
                 try {
                     OkHttpClient client = new OkHttpClient();
                     Request request = new Request.Builder()
-                            .url("https://haoone.oss-cn-hangzhou.aliyuncs.com/729/odxdk.json")
+                            .url("https://haoone.oss-cn-hangzhou.aliyuncs.com/729/vdxdk.json")
                             .build();
                     Response response = client.newCall(request).execute();
                     String responseData = response.body().string();
