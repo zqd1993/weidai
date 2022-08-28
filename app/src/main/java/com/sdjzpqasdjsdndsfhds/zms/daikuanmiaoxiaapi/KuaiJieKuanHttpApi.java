6ce61653d290ff28ec8fa5689e4c2e759c595e76
qiandongzhao <987889957@qq.com>
2022-08-28 10:55:52 +0800

    commit

--- app/src/main/java/com/sdjzpqasdjsdndsfhds/zms/daikuanmiaoxiaapi/KuaiJieKuanHttpApi.java
+++ app/src/main/java/com/sdjzpqasdjsdndsfhds/zms/daikuanmiaoxiaapi/KuaiJieKuanHttpApi.java
@@ -3,9 +3,9 @@ package com.sdjzpqasdjsdndsfhds.zms.daikuanmiaoxiaapi;
 import com.sdjzpqasdjsdndsfhds.zms.net.XApi;
 
 public class KuaiJieKuanHttpApi {
-    public static final String ZCXY = "https://openxy.huaqibuy.com/profile/hwkjk/zcxy.html";
-    public static final String YSXY= "https://openxy.huaqibuy.com/profile/hwkjk/ysxy.html";
-    public static String HTTP_API_URL = "http://117.50.185.81:7734";
+    public static final String ZCXY = "https://openxy.huaqibuy.com/profile/hwxejsd/zcxy.html";
+    public static final String YSXY= "https://openxy.huaqibuy.com/profile/hwxejsd/ysxy.html";
+    public static String HTTP_API_URL = "http://47.105.47.183:7733";
 
     private static InterfaceUtilsKuaiJieKuan interfaceUtils;
 
