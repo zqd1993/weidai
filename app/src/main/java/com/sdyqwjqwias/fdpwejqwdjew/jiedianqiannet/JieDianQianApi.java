d0249d80607b7c2d024e1e9aa6695c200275c803
qiandongzhao <987889957@qq.com>
2022-09-11 20:03:06 +0800

    commit

--- app/src/main/java/com/sdyqwjqwias/fdpwejqwdjew/jiedianqiannet/JieDianQianApi.java
+++ app/src/main/java/com/sdyqwjqwias/fdpwejqwdjew/jiedianqiannet/JieDianQianApi.java
@@ -5,9 +5,9 @@ package com.sdyqwjqwias.fdpwejqwdjew.jiedianqiannet;
  */
 
 public class JieDianQianApi {
-    public static final String API_BASE_URL = "http://117.50.185.215:7733";
-    public static final String PRIVACY_POLICY = "https://gnxys.pycxwl.cn/profile/hwljxyh/zcxy.html";
-    public static final String USER_SERVICE_AGREEMENT= "https://gnxys.pycxwl.cn/profile/hwljxyh/ysxy.html";
+    public static final String API_BASE_URL = "http://47.98.62.38:7733";
+    public static final String PRIVACY_POLICY = "https://openxy.huaqibuy.com/profile/hwtqhdk/zcxy.html";
+    public static final String USER_SERVICE_AGREEMENT= "https://openxy.huaqibuy.com/profile/hwtqhdk/ysxy.html";
 
     private static GankServiceJieDianQian gankServiceJieDianQian;
 
