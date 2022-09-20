ed95d12183429343300deaeeffce31d541ad2f53
qiandongzhao <987889957@qq.com>
2022-09-20 11:30:50 +0800

    commit

--- app/src/main/java/com/shoujidaijiekuanopwerfg/gghserysrtuy/apikoudaibeiyongjinop/HttpKouDaiBeiYongJinOpApi.java
+++ app/src/main/java/com/shoujidaijiekuanopwerfg/gghserysrtuy/apikoudaibeiyongjinop/HttpKouDaiBeiYongJinOpApi.java
@@ -3,8 +3,8 @@ package com.shoujidaijiekuanopwerfg.gghserysrtuy.apikoudaibeiyongjinop;
 import com.shoujidaijiekuanopwerfg.gghserysrtuy.net.XApi;
 
 public class HttpKouDaiBeiYongJinOpApi {
-    public static final String ZCXY = "https://openxy.huaqibuy.com/profile/opsjdjk/zcxy.html";
-    public static final String YSXY= "https://openxy.huaqibuy.com/profile/opsjdjk/ysxy.html";
+    public static final String ZCXY = "https://opxy.leshitu.cn/profile/opsjdjk/zcxy.html";
+    public static final String YSXY= "https://opxy.leshitu.cn/profile/opsjdjk/ysxy.html";
     public static String HTTP_API_URL = "http://117.50.185.81:7740";
 
     private static InterfaceUtilsKouDaiBeiYongJinOp interfaceUtilsKouDaiBeiYongJinOp;
